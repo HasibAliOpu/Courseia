972799248dc56153372c36266e6300818c4e3a6a
H_A _Opu <wwwhasib4@gmail.com>
2022-08-20 16:39:57 +0600

    simple navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-5xl">Hello ! world</h1>
+      <Navbar />
     </div>
   );
 }
