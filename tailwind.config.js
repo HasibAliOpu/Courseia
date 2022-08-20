dd3546b19449e95fd0eb260f5c37beb84ecbb023
H_A _Opu <wwwhasib4@gmail.com>
2022-08-20 16:21:14 +0600

    setup react-router, tailwindCSS and daisyUI

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#67ea88",
+
+          secondary: "#422dc6",
+
+          accent: "#be9ce5",
+
+          neutral: "#332135",
+
+          "base-100": "#FFFFFF",
+
+          info: "#2262E2",
+
+          success: "#1BD0AF",
+
+          warning: "#FAB638",
+
+          error: "#FB132E",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
