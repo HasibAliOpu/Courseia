d9954793dbb47f7070fccaa0b5ec9b98d80ed14b
H_A _Opu <wwwhasib4@gmail.com>
2022-08-21 07:25:17 +0600

    banner responsive done

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -1,25 +1,25 @@
 import React from "react";
 import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "react-router-dom";
+import { Link as a } from "react-router-dom";
 import bg from "../../../assets/banner.jpg";
 
 const Banner = () => {
   return (
     <div>
-      <div id="#banner" className="p-10 relative">
+      <div id="#banner" className="p-5 lg:p-10 relative">
         <img src={bg} alt="banner" className=" rounded-2xl" />
-        <span className="absolute bottom-48 left-24 text-5xl text-white">
+        <span className="absolute bottom-10 lg:bottom-48 left-16 lg:left-24 text-xl lg:text-5xl text-white">
           <h1>
             Take a course and <br /> improve your skills
           </h1>
-          <Link to={"/"} className="btn btn-warning btn-wide">
+          <a href={"#courses"} className="btn btn-warning btn-sm lg:btn-wide">
             Get Started{" "}
             <FontAwesomeIcon
               className="pl-2 text-2xl"
               icon={faArrowRightLong}
             />
-          </Link>
+          </a>
         </span>
       </div>
     </div>
